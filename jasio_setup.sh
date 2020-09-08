15d2989bed21d8a0673d57fb455dacdf2b7c837c
Jasioski <grabkowski.john@gmail.com>
2020-09-07 22:20:39 -0400

    Added snapd to script

--- jasio_setup.sh
+++ jasio_setup.sh
@@ -18,7 +18,7 @@ arg=$1
 
 function showHelp {
     echo "Options:"
-    echo " Choosing any of the valid options below will automatically install prerequesite software curl and wget"
+    echo " Choosing any of the valid options below will automatically install prerequesite software curl, wget, and snap package manager"
     echo "  -s, --config            To setup config files (bash, vim)"
     echo "  -h, --help              Show this help section"
     echo "  -b, --bash              To setup Bash config files (BashRC and Bash_Logout) WARNING: This will overwrite your current Bash config"
@@ -58,8 +58,10 @@ fi
 echo "Configuring machine for Jasio's setup:"
 
 # install prerequesites curl and wget
-sudo apt install curl
-sudo apt install wget
+sudo apt update
+sudo apt install -y snapd
+sudo apt install -y curl
+sudo apt install -y wget
 
 if [[ "$arg" == "-b" || "$arg" == "-a" || "$arg" == "-s" || "$arg" == "--config" || "$arg" == "--bash" || "$arg" == "--all" ]]
 then
